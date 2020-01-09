13a372cf963d5688feb1f4634e7f236ab5c4cbbc
Zprogrammer-code <skatboyza124@hotmail.com>
2020-01-09 14:55:40 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,51 @@
+require 'rubygems'
+require 'nokogiri'
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each {|key|
+  if key["status"] == "winner"
+   name = key["name"] 
+end
+return key["name"].split[0]
+}
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each { |key, value|
+  value.map { |people|
+if people["occupation"] == occupation
+  return people["name"]
+end
+}
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.each { |key, value|
+value.map { |people|
+ if people["hometown"] == hometown
+ return people["name"]
 end
-
+}
+}
+end
 def get_occupation(data, hometown)
-  # code here
+  data.each{ |key,value|
+  value.map{ |people|
+  if people["hometown"] == hometown
+    return people["occupation"]
+  end
+ }
+}
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+ 
+  data.map{ |key, value|
+value.each{ |people|
+
+ people["age"]
+}
+}
 end
